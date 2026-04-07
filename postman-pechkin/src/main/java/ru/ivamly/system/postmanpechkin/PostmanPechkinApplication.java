591c77633eb23ce3485845f2ee89f671fce64fc8
ivamly <ivamly.dev@gmail.com>
2026-04-07 17:23:23 +0300

    refac main classes

--- postman-pechkin/src/main/java/ru/ivamly/system/postmanpechkin/PostmanPechkinApplication.java
+++ postman-pechkin/src/main/java/ru/ivamly/system/postmanpechkin/PostmanPechkinApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PostmanPechkinApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(PostmanPechkinApplication.class, args);
     }
 
--- uncle-fedor/src/main/java/ru/ivamly/system/unclefedor/UncleFedorApplication.java
+++ uncle-fedor/src/main/java/ru/ivamly/system/unclefedor/UncleFedorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UncleFedorApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(UncleFedorApplication.class, args);
     }
 
