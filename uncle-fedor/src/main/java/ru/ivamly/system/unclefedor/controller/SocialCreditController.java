a656223fd56cc8b0a78102b0c15e0e07da907ec2
ivamly <ivamly.dev@gmail.com>
2026-04-07 18:57:57 +0300

    little refac and configure logging for uncle-fedor

--- uncle-fedor/src/main/java/ru/ivamly/system/unclefedor/controller/SocialCreditController.java
+++ uncle-fedor/src/main/java/ru/ivamly/system/unclefedor/controller/SocialCreditController.java
@@ -1,24 +1,47 @@
 package ru.ivamly.system.unclefedor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("/social-credit")
 public class SocialCreditController {
 
+    private static final Logger log = LoggerFactory.getLogger(SocialCreditController.class);
+
+    private final AtomicLong counter = new AtomicLong();
     private final AtomicLong socialCredit = new AtomicLong(0L);
 
     @PatchMapping("/inc")
-    public void increment() {
-        System.out.println(socialCredit.incrementAndGet());
+    public ResponseEntity<Void> increment() {
+        return updateSocialRating(AtomicLong::incrementAndGet);
     }
 
     @PatchMapping("/dec")
-    public void decrement() {
-        System.out.println(socialCredit.decrementAndGet());
+    public ResponseEntity<Void> decrement() {
+        return updateSocialRating(AtomicLong::decrementAndGet);
+    }
+
+    private ResponseEntity<Void> updateSocialRating(Consumer<AtomicLong> updater) {
+        log.trace("initial counter is {}", counter.get());
+        counter.incrementAndGet();
+        log.debug("received request № {}", counter.get());
+        if (counter.get() % 6 == 0) {
+            log.error("failed to update social credit for request № {}", counter.get());
+            return ResponseEntity.internalServerError().build();
+        }
+        updater.accept(socialCredit);
+        log.info("social credit is {} after request № {}", socialCredit.get(), counter.get());
+        if (socialCredit.get() > 10) {
+            log.warn("so bad - it's logical");
+        }
+        return ResponseEntity.ok().build();
     }
 }
